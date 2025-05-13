0e9032ed9518eea26aee2d57a65c9d30d4c69531
Tanjil Ahmed Ovi <tanzil.ovi578@gmail.com>
2025-05-13 09:07:10 +0600

    template folder issue

--- pehchano/settings.py
+++ pehchano/settings.py
@@ -83,7 +83,7 @@ LOGIN_REDIRECT_URL = '/'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR, 'templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
